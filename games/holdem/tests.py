1b33cdfc365f7cd36170f99ffac37dddc39ace1a
Nikita Zhukov <nikita@flexites.org>
2020-02-27 19:18:53 +0500

    Extended tests for high card, pair and two pair

--- games/holdem/tests.py
+++ games/holdem/tests.py
@@ -15,19 +15,74 @@ class IdentifyTestCase(unittest.TestCase):
         del self.deck
 
     def test_high_card(self):
+        # High card in the pocket
         pocket = Pocket(self.deck['As'], self.deck['2s'])
         board = Board(self.deck['3s'], self.deck['4d'], self.deck['Jh'], self.deck['Qd'], self.deck['6s'])
-        self.assertIsInstance(Hands.identify(pocket, board), HighCard)
+        hand = Hands.identify(pocket, board)
+        self.assertIsInstance(hand, HighCard)
+        self.assertEqual(hand[0], self.deck['As'])
+        self.assertEqual(hand[1], self.deck['Qd'])
+
+        # High card on the board
+        pocket = Pocket(self.deck['7s'], self.deck['2s'])
+        board = Board(self.deck['3s'], self.deck['4d'], self.deck['Jh'], self.deck['Qd'], self.deck['6s'])
+        hand = Hands.identify(pocket, board)
+        self.assertIsInstance(hand, HighCard)
+        self.assertEqual(hand[0], self.deck['Qd'])
+        self.assertEqual(hand[1], self.deck['Jh'])
 
     def test_one_pair(self):
+        # One pair, in the pocket and on the board
         pocket = Pocket(self.deck['As'], self.deck['2s'])
         board = Board(self.deck['3s'], self.deck['Ad'], self.deck['Jh'], self.deck['Qd'], self.deck['6s'])
-        self.assertIsInstance(Hands.identify(pocket, board), OnePair)
+        hand = Hands.identify(pocket, board)
+        self.assertIsInstance(hand, OnePair)
+        self.assertEqual(set(hand[0:2]), set([ self.deck['As'], self.deck['Ad'], ]))
+        self.assertEqual(hand[2], self.deck['Qd'])
+
+        # One pair, on the board only
+        pocket = Pocket(self.deck['As'], self.deck['2s'])
+        board = Board(self.deck['3s'], self.deck['4d'], self.deck['Jh'], self.deck['Qd'], self.deck['4s'])
+        hand = Hands.identify(pocket, board)
+        self.assertIsInstance(hand, OnePair)
+        self.assertEqual(set(hand[0:2]), set([ self.deck['4d'], self.deck['4s'], ]))
+        self.assertEqual(hand[2], self.deck['As'])
+
+        # One pair, in the pocket only
+        pocket = Pocket(self.deck['As'], self.deck['Ad'])
+        board = Board(self.deck['3s'], self.deck['4d'], self.deck['Jh'], self.deck['Qd'], self.deck['6s'])
+        hand = Hands.identify(pocket, board)
+        self.assertIsInstance(hand, OnePair)
+        self.assertEqual(set(hand[0:2]), set([ self.deck['As'], self.deck['Ad'], ]))
+        self.assertEqual(hand[2], self.deck['Qd'])
 
     def test_two_pair(self):
+        # Two pair, in the pocket and on the board (mixed)
         pocket = Pocket(self.deck['As'], self.deck['2s'])
-        board = Board(self.deck['3s'], self.deck['Ad'], self.deck['3h'], self.deck['Qd'], self.deck['6s'])
-        self.assertIsInstance(Hands.identify(pocket, board), TwoPair)
+        board = Board(self.deck['3s'], self.deck['Ad'], self.deck['2h'], self.deck['Qd'], self.deck['6s'])
+        hand = Hands.identify(pocket, board)
+        self.assertIsInstance(hand, TwoPair)
+        self.assertEqual(set(hand[0:2]), set([ self.deck['As'], self.deck['Ad'], ]))
+        self.assertEqual(set(hand[2:4]), set([ self.deck['2s'], self.deck['2h'], ]))
+        self.assertEqual(hand[4], self.deck['Qd'])
+
+        # Two pair, in the pocket and on the board (separetely)
+        pocket = Pocket(self.deck['As'], self.deck['Ad'])
+        board = Board(self.deck['3s'], self.deck['3d'], self.deck['Jh'], self.deck['Qd'], self.deck['6s'])
+        hand = Hands.identify(pocket, board)
+        self.assertIsInstance(hand, TwoPair)
+        self.assertEqual(set(hand[0:2]), set([ self.deck['As'], self.deck['Ad'], ]))
+        self.assertEqual(set(hand[2:4]), set([ self.deck['3s'], self.deck['3d'], ]))
+        self.assertEqual(hand[4], self.deck['Qd'])
+
+        # Two pair, on the board only
+        pocket = Pocket(self.deck['As'], self.deck['2s'])
+        board = Board(self.deck['3s'], self.deck['3d'], self.deck['Jh'], self.deck['Qd'], self.deck['Qs'])
+        hand = Hands.identify(pocket, board)
+        self.assertIsInstance(hand, TwoPair)
+        self.assertEqual(set(hand[0:2]), set([ self.deck['Qd'], self.deck['Qs'], ]))
+        self.assertEqual(set(hand[2:4]), set([ self.deck['3s'], self.deck['3d'], ]))
+        self.assertEqual(hand[4], self.deck['As'])
 
     def test_three_of_a_kind(self):
         pocket = Pocket(self.deck['As'], self.deck['2s'])
