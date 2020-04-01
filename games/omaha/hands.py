7362139deb4bde7974fcf73a748845d514940db8
Nikita N. Zhukov <nikitanickzhukov@gmail.com>
2020-04-01 13:07:53 +0300

    Omaha hands' precheck methods are implemented

--- games/omaha/hands.py
+++ games/omaha/hands.py
@@ -1,44 +1,158 @@
 from itertools import combinations, product
+from collections import Counter
 
+from cards import ranks
 from ..base import hands
 from .pockets import Pocket
 from .boards import Board
 
 
+min_rank = min(ranks)
+max_rank = max(ranks)
+
+
 class HighCard(hands.HighCard):
-    pass
+    @classmethod
+    def precheck(cls, pocket:Pocket, board:Board) -> bool:
+        return True
 
 
 class OnePair(hands.OnePair):
-    pass
+    @classmethod
+    def precheck(cls, pocket:Pocket, board:Board) -> bool:
+        ranks = [ x.rank for x in pocket ] + [ y.rank for y in board ]
+        counter = Counter(ranks)
+        for rank, count in counter.most_common(1):
+            if count >= 2:
+                return True
+            else:
+                break
+        return False
 
 
 class TwoPair(hands.TwoPair):
-    pass
+    @classmethod
+    def precheck(cls, pocket:Pocket, board:Board) -> bool:
+        ranks = [ x.rank for x in pocket ] + [ y.rank for y in board ]
+        counter = Counter(ranks)
+        groups = []
+        for rank, count in counter.most_common(4):
+            if count >= 2:
+                groups.append(rank)
+            else:
+                break
+        pocket_count = len([ True for x in pocket if x.rank in groups ])
+        if pocket_count >= 1:
+            board_count = len([ True for x in board if x.rank in groups ])
+            if board_count >= 2:
+                return True
+        return False
 
 
 class Trips(hands.Trips):
-    pass
+    @classmethod
+    def precheck(cls, pocket:Pocket, board:Board) -> bool:
+        ranks = [ x.rank for x in pocket ] + [ y.rank for y in board ]
+        counter = Counter(ranks)
+        for rank, count in counter.most_common(3):
+            if count >= 3:
+                pocket_count = len([ True for x in pocket if x.rank == rank ])
+                if count - pocket_count >= 1:
+                    return True
+            else:
+                break
+        return False
 
 
 class Straight(hands.Straight):
-    pass
+    @classmethod
+    def precheck(cls, pocket:Pocket, board:Board) -> bool:
+        ranks = sorted(set([ x.rank for x in pocket ] + [ y.rank for y in board ]), reverse=True)
+
+        cur_seq = [ranks[0]]
+        all_seq = []
+        for i in range(1, len(ranks)):
+            if ranks[i].weight == ranks[i - 1].weight - 1:
+                cur_seq.append(ranks[i])
+            else:
+                if len(cur_seq) >= 4:
+                    all_seq.append(cur_seq)
+                cur_seq = [ranks[i]]
+        if len(cur_seq) >= 4:
+            all_seq.append(cur_seq)
+
+        for seq in all_seq:
+            if seq[-1] == min_rank and max_rank in ranks:
+                # Wheel straight is found
+                seq.append(max_rank)
+
+        all_seq = [ x for x in all_seq if len(x) >= 5 ]
+
+        for seq in all_seq:
+            pocket_count = len(set([ x.rank for x in pocket if x.rank in seq ]))
+            if pocket_count >= 2:
+                board_count = len(set([ x.rank for x in board if x.rank in seq ]))
+                if board_count >= 3:
+                    return True
+        return False
 
 
 class Flush(hands.Flush):
-    pass
+    @classmethod
+    def precheck(cls, pocket:Pocket, board:Board) -> bool:
+        suits = [ x.suit for x in pocket ] + [ y.suit for y in board ]
+        counter = Counter(suits)
+        for suit, count in counter.most_common(1):
+            if count >= 5:
+                pocket_count = len([ True for x in pocket if x.suit == suit ])
+                if pocket_count >= 2 and count - pocket_count >= 3:
+                    return True
+            else:
+                break
+        return False
 
 
 class FullHouse(hands.FullHouse):
-    pass
+    @classmethod
+    def precheck(cls, pocket:Pocket, board:Board) -> bool:
+        ranks = [ x.rank for x in pocket ] + [ y.rank for y in board ]
+        counter = Counter(ranks)
+        groups = []
+        for rank, count in counter.most_common():
+            if count >= 3:
+                groups.append(rank)
+            elif groups and count >= 2:
+                groups.append(rank)
+            else:
+                break
+        if len(groups) >= 2:
+            pocket_count = len([ True for x in pocket if x.rank in groups ])
+            if pocket_count >= 2:
+                board_count = len([ True for x in board if x.rank in groups ])
+                if board_count >= 3:
+                    return True
+        return False
 
 
 class Quads(hands.Quads):
-    pass
+    @classmethod
+    def precheck(cls, pocket:Pocket, board:Board) -> bool:
+        ranks = [ x.rank for x in pocket ] + [ y.rank for y in board ]
+        counter = Counter(ranks)
+        for rank, count in counter.most_common(2):
+            if count >= 4:
+                pocket_count = len([ True for x in pocket if x.rank == rank ])
+                if count - pocket_count >= 2:
+                    return True
+            else:
+                break
+        return False
 
 
 class StraightFlush(hands.StraightFlush):
-    pass
+    @classmethod
+    def precheck(cls, pocket:Pocket, board:Board) -> bool:
+        return Flush.precheck(pocket, board) and Straight.precheck(pocket, board)
 
 
 class Hands(hands.Hands):
