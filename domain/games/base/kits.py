4aa2eae036ed716a397059d17351dddd6b0f8c43
Nikita N. Zhukov <nikitanickzhukov@gmail.com>
2020-05-13 17:20:52 +0300

    Modules are restructured

---
+++ domain/games/base/kits.py
@@ -0,0 +1,48 @@
+from typing import Optional, Sequence
+from abc import ABC
+
+from domain.cards import Card
+
+from .streets import Street
+
+
+class Kit(ABC):
+    """
+    Representation of abstract kit (a parent class for board and pocket)
+    """
+
+    __slots__ = ('_streets',)
+
+    def __init__(self, streets: Optional[Sequence[Street]] = None) -> None:
+        self._streets = streets or []
+
+    def __repr__(self) -> str:
+        if not self._streets:
+            return '<{}: empty>'.format(self.__class__.__name__)
+        return '<{}: {}>'.format(self.__class__.__name__, str(self))
+
+    def __str__(self) -> str:
+        return str([str(x) for x in self.cards])
+
+    def __contains__(self, item: Card) -> bool:
+        return item in self.cards
+
+    def __len__(self) -> int:
+        return len(self.cards)
+
+    def __iter__(self) -> iter:
+        return iter(self.cards)
+
+    def append(self, street: Street) -> None:
+        self._streets.append(street)
+
+    @property
+    def streets(self) -> Sequence[Street]:
+        return tuple(self._streets)
+
+    @property
+    def cards(self) -> Sequence[Card]:
+        return tuple(x for s in self._streets for x in s)
+
+
+__all__ = ('Kit',)
---
+++ tests/domain/games/holdem/hands/test_flush.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+
+from domain.cards import StandardDeck
+from domain.games.holdem import (
+    Preflop, Flop, Turn, River,
+    Pocket, Board, Identifier, Flush
+)
+
+
+class FlushTestCase(TestCase):
+    def test_2_pocket_3_board(self):
+        deck = StandardDeck()
+        preflop = Preflop(cards=deck.extract(('As', '2s')))
+        pocket = Pocket(streets=(preflop,))
+        flop = Flop(cards=deck.extract(('3s', 'Ad', 'Ac')))
+        turn = Turn(cards=deck.extract(('Qs',)))
+        river = River(cards=deck.extract(('6s',)))
+        board = Board(streets=(flop, turn, river))
+        hand = Identifier.identify(pocket, board)
+        self.assertIsInstance(hand, Flush)
+        self.assertEqual([x.code for x in hand], ['As', 'Qs', '6s', '3s', '2s'])
+
+    def test_1_pocket_4_board(self):
+        deck = StandardDeck()
+        preflop = Preflop(cards=deck.extract(('As', '2s')))
+        pocket = Pocket(streets=(preflop,))
+        flop = Flop(cards=deck.extract(('3s', 'Ad', 'Js')))
+        turn = Turn(cards=deck.extract(('Qs',)))
+        river = River(cards=deck.extract(('6s',)))
+        board = Board(streets=(flop, turn, river))
+        hand = Identifier.identify(pocket, board)
+        self.assertIsInstance(hand, Flush)
+        self.assertEqual([x.code for x in hand], ['As', 'Qs', 'Js', '6s', '3s'])
+
+    def test_5_board(self):
+        deck = StandardDeck()
+        preflop = Preflop(cards=deck.extract(('As', '2s')))
+        pocket = Pocket(streets=(preflop,))
+        flop = Flop(cards=deck.extract(('3d', 'Ad', 'Jd')))
+        turn = Turn(cards=deck.extract(('Qd',)))
+        river = River(cards=deck.extract(('6d',)))
+        board = Board(streets=(flop, turn, river))
+        hand = Identifier.identify(pocket, board)
+        self.assertIsInstance(hand, Flush)
+        self.assertEqual([x.code for x in hand], ['Ad', 'Qd', 'Jd', '6d', '3d'])
